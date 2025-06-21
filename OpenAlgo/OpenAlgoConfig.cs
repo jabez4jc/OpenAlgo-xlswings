9a9c47a235994385ab94edf2a49885c1c180d552
jabez4jc <37698296+jabez4jc@users.noreply.github.com>
2025-06-22 04:22:22 +0530

    Delete OpenAlgo directory

--- OpenAlgo/OpenAlgoConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenAlgo
-{
-    public static class OpenAlgoConfig
-    {
-        public static string ApiKey { get; set; } = "";
-        public static string Version { get; set; } = "v1";
-        public static string HostUrl { get; set; } = "http://127.0.0.1:5000";
-    }
-}
